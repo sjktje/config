58c54b0d6d250355e189fd6ae3672f2bf7586807
Svante Kvarnström <sjk@ankeborg.nu>
2016-06-07 06:54:00 +0200

    Add hammerspoon config

---
+++ home/hammerspoon/init.lua
@@ -0,0 +1,14 @@
+local tiling = require "hs.tiling"
+local hotkey = require "hs.hotkey"
+local mash = {"ctrl", "cmd"}
+
+hotkey.bind(mash, "c", function() tiling.cycleLayout() end)
+hotkey.bind(mash, "j", function() tiling.cycle(1) end)
+hotkey.bind(mash, "k", function() tiling.cycle(-1) end)
+hotkey.bind(mash, "space", function() tiling.promote() end)
+hotkey.bind(mash, "f", function() tiling.goToLayout("fullscreen") end)
+
+-- If you want to set the layouts that are enabled
+tiling.set('layouts', {
+  'fullscreen', 'main-vertical'
+})
