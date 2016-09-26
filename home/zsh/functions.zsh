c5f2b384508ecfce34b7452274027136d6b11205
Svante Kvarnström <sjk@ankeborg.nu>
2016-09-26 16:33:34 +0200

    [zsh] add wgetfilewait function

--- home/zsh/functions.zsh
+++ home/zsh/functions.zsh
@@ -44,3 +44,8 @@ function code() {
         workon $venv
     fi
 }
+
+function wgetfilewait() {
+    USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+    $(wget --random-wait --wait=${1:-2m} -i ${2:-download.txt} -U ${USER_AGENT})
+}
