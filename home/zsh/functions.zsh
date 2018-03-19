9074b9b80540a22c66c32481986411a8e92e63ad
Svante Kvarnström <sjk@ankeborg.nu>
2018-03-19 12:08:03 +0100

    [zsh] add gtd and cimt functions

--- home/zsh/functions.zsh
+++ home/zsh/functions.zsh
@@ -7,6 +7,8 @@ function bytetombit() { echo $(( $1/1024.0**2*8 )) }
 function mbittobyte() { echo $(( $1*1024.0**2/8 )) }
 function texpdf() { pdflatex ${1}.tex && open ${1}.pdf }
 function cl() { cd ${1} && ls }
+function gtd() { cd ~/Documents/gtd && vim $(date '+%Y-%W.otl') }
+function cimt { git commit -m "[${1}] $*[2,-1]" }
 function 6p() {
 	ret=$(curl -s -F "content=<${1--}" -F ttl=604800 -w "%{redirect_url}\n" -o /dev/null https://p.6core.net/;)
 
